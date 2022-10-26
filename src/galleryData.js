a18967ef94cbe92c994004ecad388b93c32807c4
“Holly <hollyefig@gmail.com>
2022-10-26 10:28:10 -0400

    updated w new tractive images

--- src/galleryData.js
+++ src/galleryData.js
@@ -13,10 +13,10 @@ import vector1 from "./galleryData/vector1.png";
 import vector2 from "./galleryData/vector2.png";
 import vector3 from "./galleryData/vector3.png";
 import vector4 from "./galleryData/vector4.jpeg";
-import tractive1 from "./galleryData/tractive1.jpg";
-import tractive2 from "./galleryData/tractive2.jpg";
-import tractive3 from "./galleryData/tractive3.jpg";
-import tractive4 from "./galleryData/tractive4.jpg";
+import tractive1 from "./galleryData/tractive1Phone.png";
+import tractive2 from "./galleryData/tractive2Phone.png";
+import tractive3 from "./galleryData/tractive3Phone.png";
+import tractive4 from "./galleryData/tractive4Phone.png";
 export const GalleryData = [
   {
     id: 1,
@@ -42,12 +42,12 @@ export const GalleryData = [
 
   {
     id: 3,
-    name: "Art Guide",
-    preview: wiiuaaPrev,
+    name: "Tractive App",
+    preview: [tractivePrev],
     description:
-      "Art Academy is a downloadable app for the Nintendo WiiU gaming console. The app is a sketchpad and coloring tool for users to create art on. However, it provides no instructions on how to use, which has lead many users to confusion on where to start. This website provides some basic step by steps on how to utilize the tools, build up layers, and properly export their artwork online.",
-    display: [wiiuaaDisplay],
-    url: "http://hollyfig.com/artacademytutorial/index.html",
+      "A mockup for a client. Tractive is a tracker app for parents to use on their phone which can track a child's active and inactive time. The child gets rewarded time for video game activity if they input the appropriate physical activity, and monitor when a child has overdone their time on gaming.",
+    display: [tractive1, tractive2, tractive3, tractive4],
+    url: false,
     github: false,
   },
 
@@ -64,12 +64,12 @@ export const GalleryData = [
 
   {
     id: 5,
-    name: "Tractive App",
-    preview: [tractivePrev],
+    name: "Art Guide",
+    preview: wiiuaaPrev,
     description:
-      "A mockup for a client. Tractive is a tracker app for parents to use on their phone which can track a child's active and inactive time. The child gets rewarded time for video game activity if they input the appropriate physical activity, and monitor when a child has overdone their time on gaming.",
-    display: [tractive1, tractive2, tractive3, tractive4],
-    url: false,
+      "Art Academy is a downloadable app for the Nintendo WiiU gaming console. The app is a sketchpad and coloring tool for users to create art on. However, it provides no instructions on how to use, which has lead many users to confusion on where to start. This website provides some basic step by steps on how to utilize the tools, build up layers, and properly export their artwork online.",
+    display: [wiiuaaDisplay],
+    url: "http://hollyfig.com/artacademytutorial/index.html",
     github: false,
   },
 ];
